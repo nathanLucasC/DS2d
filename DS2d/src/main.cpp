087ae826623334375c2cfc44dc4fb92fb5453a4c
Bob <miguel.camilleri1@gmail.com>
2025-06-11 17:53:52 +0100

    Added include & src/framework to project structure to break up and seperate code

--- DS2d/src/main.cpp
+++ DS2d/src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Dark Souls 2Ds!" << std::endl;
+    sf::RenderWindow renderWin(sf::VideoMode(800, 600), "Win");
+
+    while (renderWin.isOpen())
+    {
+        sf::Event winEvent;
+        while (renderWin.pollEvent(winEvent)) 
+        {
+            if (winEvent.type == sf::Event::EventType::Closed) 
+            {
+                renderWin.close();
+            }
+        }
+    }
+    std::cout << "Build Successful. Application closed." << std::endl; 
 }
